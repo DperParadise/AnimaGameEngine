220e014b0d272aadaabf748c4732bfee13678c19
Daniel Pérez <dperezparaiso@gmail.com>
2017-11-10 16:55:12 +0100

    Fix. Using importer singleton in model load.

--- AnimaGameEngine/Model.cpp
+++ AnimaGameEngine/Model.cpp
@@ -1,3 +1,4 @@
+#include "Importer.h"
 #include "Model.h"
 #include "libraries/assimp/include/assimp/scene.h"
 #include "libraries/assimp/include/assimp/postprocess.h"
@@ -15,7 +16,7 @@ Model::~Model() {}
 
 void Model::Load(std::string name, const char *file)
 {
-	scene = importer.ReadFile(file,  /*aiPostProcessSteps::aiProcess_PreTransformVertices |*/ 
+	scene = importer->GetInstance()->ReadFile(file,  /*aiPostProcessSteps::aiProcess_PreTransformVertices |*/ 
 		aiPostProcessSteps::aiProcess_FlipUVs |
 		aiPostProcessSteps::aiProcess_Triangulate /*| 
 	aiProcessPreset_TargetRealtime_Fast*/);
@@ -53,52 +54,3 @@ void Model::LoadHierarchy(aiNode *node, GameObject *go, const char *file)
 		go->CreateMaterialComp(scene->mMeshes[mesh_index], scene, file);
 	}
 }
-
-//void Model::Draw()
-//{
-//
-//
-//
-//
-//	aiString texture_file;
-//	glEnableClientState(GL_VERTEX_ARRAY);
-//	glEnableClientState(GL_NORMAL_ARRAY);
-//	if (num_textures != 0)
-//	{
-//		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-//	}
-//
-//	for (uint i = 0; i < scene->mNumMeshes; i++)
-//	{	
-//		uint mat_index = scene->mMeshes[i]->mMaterialIndex;
-//		if (scene->mMaterials[mat_index]->GetTextureCount(aiTextureType::aiTextureType_DIFFUSE) != 0)
-//		{
-//			scene->mMaterials[mat_index]->GetTexture(aiTextureType::aiTextureType_DIFFUSE, 0, &texture_file);
-//			std::string texture_path(root_path);
-//			unsigned int tex_id = textureManager->GetInstance()->Load(aiString(texture_path.append(texture_file.data)));
-//			glBindTexture(GL_TEXTURE_2D, tex_id);
-//			glTexCoordPointer(2, GL_FLOAT, 0, uv_array[i]);
-//		}
-//
-//		glVertexPointer(3, GL_FLOAT, 0, vertex_array[i]);	
-//		glNormalPointer(GL_FLOAT, 0, normal_array[i]);
-//
-//		glMaterialfv(GL_FRONT, GL_AMBIENT, materials[i].ambient);
-//		glMaterialfv(GL_FRONT, GL_DIFFUSE, materials[i].diffuse);
-//		glMaterialfv(GL_FRONT, GL_SPECULAR, materials[i].specular);
-//		glMaterialf(GL_FRONT, GL_SHININESS, materials[i].shininess);
-//
-//		uint num_verts = scene->mMeshes[i]->mNumFaces * 3;
-//		glDrawArrays(GL_TRIANGLES, 0, num_verts);	
-//	}
-//
-//	glDisableClientState(GL_VERTEX_ARRAY);
-//	glDisableClientState(GL_NORMAL_ARRAY);
-//	if (num_textures != 0)
-//	{
-//		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-//	}
-//	glBindTexture(GL_TEXTURE_2D, 0);	
-//}
-
-void Model::Clear() {}
--- AnimaGameEngine/Model.h
+++ AnimaGameEngine/Model.h
@@ -1,9 +1,8 @@
 #ifndef __MODEL_H__
 #define __MODEL_H__
 
-#include "libraries/assimp/include/assimp/Importer.hpp"
 #include <string>
-
+class Importer;
 struct aiScene;
 struct aiNode;
 class GameObject;
@@ -14,15 +13,12 @@ public:
 	Model(std::string name, const char *file);
 	~Model();
 
-	//void Draw();
-	void Clear();
-
 	std::string model_name;
 	GameObject *model_go = nullptr;
 	
 private:
 	const aiScene *scene;
-	Assimp::Importer importer;	
+	Importer *importer = nullptr;
 	void Load(std::string name, const char *file);
 	void LoadHierarchy(aiNode *node, GameObject *go, const char *file);
 };

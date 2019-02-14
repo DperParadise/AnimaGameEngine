8d1cb3f96f26a841b258217f30819ea8d9f0502f
Daniel <dperezparaiso@gmail.com>
2019-02-14 15:12:13 +0100

    Deleted unused methods. Reordered methods in the header.

--- AnimaGameEngine/GameObject.cpp
+++ AnimaGameEngine/GameObject.cpp
@@ -39,9 +39,6 @@ void GameObject::Update(float dt)
 	{
 		(*it)->Update(dt);
 	}
-
-	//if (selectedGO)
-	//	gizmo.Draw(this);
 }
 
 void GameObject::UpdateWorldTransform()
@@ -65,17 +62,7 @@ void GameObject::UpdateWorldTransform()
 	dirty = false;
 }
 
-void GameObject::Translate(const glm::vec3 & pos)
-{
-	transform->Translate(pos);
-	dirty = true;
-}
 
-void GameObject::Scale(const glm::vec3 & scale)
-{
-	transform->Scale(scale);
-	dirty = true;
-}
 
  ComponentTransform* GameObject::GetTransform()
 {
@@ -87,40 +74,79 @@ void GameObject::Scale(const glm::vec3 & scale)
 	 return transform;
  }
 
+
+
+const std::string & GameObject::GetName() const
+{
+	return name;
+}
+
+const std::vector<GameObject*>& GameObject::GetChildrenGO() const
+{
+	return childrenGO;
+}
+
+GameObject * GameObject::GetParentGO() const
+{
+	return parentGO;
+}
+
+void GameObject::SetParentGO(GameObject * parentGO)
+{
+	this->parentGO = parentGO;
+}
+
+void GameObject::AddChildGO(GameObject *childGO)
+{
+	if (childGO)
+		childrenGO.push_back(childGO);
+}
+
+
+void GameObject::SetDirty()
+{
+	dirty = true;
+}
+bool GameObject::IsDirty() const
+{
+	return dirty;
+}
+
+const std::vector<Component*>& GameObject::GetComponents() const
+{
+	return components;
+}
+
+Component * GameObject::FindComponentByType(ComponentType type)
+{
+	Component *found = nullptr;
+	for (Component *component : components)
+	{
+		if (component->GetComponentType() == type)
+			found = component;
+	}
+	return found;
+}
+
+void GameObject::Translate(const glm::vec3 & pos)
+{
+	transform->Translate(pos);
+	dirty = true;
+}
+
 void GameObject::Rotate(float angle, const glm::vec3 &axis)
 {
 	transform->Rotate(angle, axis);
 	dirty = true;
 }
-/*
-Component* GameObject::CreatePrimitiveMeshComp(ComponentMaterial *mat, float *vertices, float *normals, float *uv)
+
+void GameObject::Scale(const glm::vec3 & scale)
 {
-	Component *comp = new ComponentMesh(ComponentType::MESH, mat, std::string("Mesh"), true, this, vertices, normals, uv);
-	components.push_back(comp);
-	return comp;
+	transform->Scale(scale);
+	dirty = true;
 }
 
-Component* GameObject::CreatePrimitiveMatComp(float *ambient, float *diffuse, float *specular, float shininess)
-{
-	Component *comp = new ComponentMaterial(ComponentType::MATERIAL, std::string("Material"), true, this, ambient, diffuse, specular, shininess);
-	components.push_back(comp);
-	return comp;
-}
 
-Component* GameObject::CreateLoadedMeshComp(ComponentMaterial *mat, aiMesh *mesh)
-{
-	Component *comp = new ComponentMesh(ComponentType::MATERIAL, mat, std::string("Mesh"), true, this, mesh);
-	components.push_back(comp);
-	return comp;
-}
-
-Component* GameObject::CreateLoadedMaterialComp(aiMesh *mesh, const aiScene *scene, const char *file_name)
-{
-	Component *comp = new ComponentMaterial(ComponentType::MATERIAL, std::string("Material"), true, this, mesh, scene, file_name);
-	components.push_back(comp);
-	return comp;
-}
-*/
 Component* GameObject::AddMeshRenderer(const Mesh *mesh, const Shader *shader, const ComponentCamera *camera)
 {
 	Component *comp = new ComponentMeshRenderer(ComponentType::MESH_RENDERER, mesh, shader, camera, this);
@@ -148,104 +174,16 @@ Component * GameObject::AddEditorCameraComponent()
 	components.push_back(comp);
 	return comp;
 }
-/*
-Component* GameObject::CreateBehaviour(const std::string &behav_name)
-{
-	Component *comp = new ComponentBehaviour(ComponentType::BEHAVIOUR, behav_name, std::string("Behaviour"), true, this);
-	components.push_back(comp);
-	return comp;
-}
 
-Component* GameObject::CreateTorsoBehaviour(const std::string &behav_name)
-{
-	Component *comp = new ComponentTorsoBehaviour(ComponentType::BEHAVIOUR, behav_name, std::string("TorsoBehaviour"), true, this);
-	components.push_back(comp);
-	return comp;
-}
 
-Component *GameObject::CreateAmbientLight()
-{
-	Component *comp = new ComponentAmbientLight(ComponentType::AMBIENT_LIGHT, std::string("Ambient Light"), true, this);
-	components.push_back(comp);
-	return comp;
-}
-
-Component *GameObject::CreatePointLight()
-{
-	Component *comp = new ComponentLight(ComponentType::POINT_LIGHT, std::string("Point Light"), true, this);
-	components.push_back(comp);
-	return comp;
-}
-*/
-const std::string & GameObject::GetName() const
-{
-	return name;
-}
-
-const std::vector<GameObject*>& GameObject::GetChildrenGO() const
-{
-	return childrenGO;
-}
-
-GameObject * GameObject::GetParentGO() const
-{
-	return parentGO;
-}
-
-void GameObject::SetParentGO(GameObject * parentGO)
-{
-	this->parentGO = parentGO;
-}
-
-void GameObject::AddChildGO(GameObject *childGO)
-{
-	if (childGO)
-		childrenGO.push_back(childGO);
-}
-
-Component * GameObject::FindComponentByType(ComponentType type)
-{
-	Component *found = nullptr;
-	for (Component *component : components)
-	{
-		if (component->GetComponentType() == type)
-			found = component;
-	}
-	return found;
-}
-void GameObject::SetDirty()
-{
-	dirty = true;
-}
-bool GameObject::IsDirty() const
-{
-	return dirty;
-}
-
-const std::vector<Component*>& GameObject::GetComponents() const
-{
-	return components;
-}
-
-/*
-Component *GameObject::CreateDirectionalLight()
-{
-	Component *comp = new ComponentLight(ComponentType::DIRECTIONAL_LIGHT, std::string("Directional Light"), true, this);
-	components.push_back(comp);
-	return comp;
-}
-*/
 void GameObject::LoadTransform(const aiNode *node)
 {
-	//Assimp types
 	aiVector3D pos;
 	aiVector3D scale;
 	aiQuaternion rot;
 
 	node->mTransformation.Decompose(scale, rot, pos);
 
-	// TODO: override asignment operator to assign between glm and assimp types
-	//glm types
 	glm::vec3 posGLM(pos.x, pos.y, pos.z);
 	glm::vec3 scaleGLM(scale.x, scale.y, scale.z);
 	glm::quat rotGLM(rot.x, rot.y, rot.z, rot.w);
--- AnimaGameEngine/GameObject.h
+++ AnimaGameEngine/GameObject.h
@@ -4,72 +4,54 @@
 #include <string>
 #include <vector>
 #include "Component.h"
-//#include "Gizmo.h"
 #include "libraries/glm/glm.hpp"
 
-
 struct aiNode;
-struct aiMesh;
-struct aiScene;
-//class ComponentMaterial;
 class ComponentTransform;
 class Mesh;
 class Shader;
 class ComponentCamera;
-class ModuleEditorCamera;
 
 class GameObject
 {
 public:
-
 	GameObject(const std::string &name, const aiNode *node = nullptr);
 	virtual ~GameObject();
 
 	virtual void Update(float dt);
 	void UpdateWorldTransform();
 
-	void Translate(const glm::vec3 &translation);
-	void Rotate(float angle, const glm::vec3 &axis);
-	void Scale(const glm::vec3 &scale);
-
+	///Getters and setters
 	ComponentTransform *GetTransform();
 	const ComponentTransform *GetTransform() const;
-
-	//Component *CreatePrimitiveMeshComp(ComponentMaterial *mat, float *vertices, float *normals, float *uv);
-	//Component *CreatePrimitiveMatComp(float *ambient, float *diffuse, float *specular, float shininess);
-
-	//Component* CreateLoadedMeshComp(ComponentMaterial *mat, aiMesh *mesh);
-	//Component* CreateLoadedMaterialComp(aiMesh *mesh, const aiScene *scene, const char *file_name);
-	Component* AddMeshRenderer(const Mesh *mesh, const Shader *shader, const ComponentCamera *camera);
-	Component* AddCameraComponent();
-	Component* AddEditorCameraComponent();
-	Component* AddGizmoComponent(const std::string& vertexPath, const std::string &fragmentPath);
-	//Component* CreateBehaviour(const std::string &behav_name);
-	//Component* CreateTorsoBehaviour(const std::string &behav_name);
-	//Component* CreateAmbientLight();
-	//Component* CreateDirectionalLight();
-	//Component* CreatePointLight();
-
 	const std::string& GetName() const;
 	const std::vector<GameObject*>& GetChildrenGO() const;
-
 	GameObject *GetParentGO() const;
 	void SetParentGO(GameObject *parentGO);
-
 	void AddChildGO(GameObject *childGO);
 	
+	const std::vector<Component*>& GetComponents() const;
 	Component *FindComponentByType(ComponentType type);
 	
 	void SetDirty();
 	bool IsDirty() const;
 
-	//GUI can modify this value
+	///GUI can modify this value
 	bool enabled = true;
 
-	const std::vector<Component*>& GetComponents() const;
+
+	///Methods to modify the transform
+	void Translate(const glm::vec3 &translation);
+	void Rotate(float angle, const glm::vec3 &axis);
+	void Scale(const glm::vec3 &scale);
+
+	///Methods to Add components
+	Component* AddMeshRenderer(const Mesh *mesh, const Shader *shader, const ComponentCamera *camera);
+	Component* AddCameraComponent();
+	Component* AddEditorCameraComponent();
+	Component* AddGizmoComponent(const std::string& vertexPath, const std::string &fragmentPath);
 
 private:
-
 	ComponentTransform *transform = nullptr;
 
 	bool dirty = true;
@@ -78,12 +60,9 @@ private:
 	std::vector<Component*> components;
 	std::vector<GameObject*> childrenGO;
 
-	bool selectedGO = true;
-
 	void LoadTransform(const aiNode *node);
 	void CombineTransform(GameObject *parentGO);
 	void Clear();
-	//Gizmo gizmo;
 };
 
 
